ab206b1888f64ee7d36c405f68960b0ca7ccbad2
eftelakacori <eftelakacori92@gmail.com>
2024-11-25 18:02:49 +0000

    Matrica 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,9 +4,8 @@
 
 def matrix_shape(matrix):
     """ returns list of integers representing dimensions of given matrix"""
-    matrix_shape=[]
-    while (type(matrix)is list):
+    matrix_shape = []
+    while (type(matrix) is list):
         matrix_shape.append(len(matrix))
         matrix = matrix[0]
     return matrix_shape
-    
