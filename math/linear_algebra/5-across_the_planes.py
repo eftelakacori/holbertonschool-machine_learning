7be8b508c30b80788d8655e08943d2adc5a35245
eftelakacori <eftelakacori92@gmail.com>
2024-11-27 23:29:14 +0000

    adds two matrices element-wise

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-"""defines function 2D matrices containing ints/floats"""
+"""defines function that adds two matrices element-wise"""
 
 
-def add_matrices2D(mat1, mat2):git
-    # Kontrollo nëse matricat kanë të njëjtën formë
+def add_matrices2D(mat1, mat2):
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
-        return None    
-    # Kthe matricën që është shuma element-për-element
+        return None
     return [
         [mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
         for i in range(len(mat1))
