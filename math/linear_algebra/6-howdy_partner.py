fad8408c5b7f05d2848a6ecda739b19b878653cd
eftelakacori <eftelakacori92@gmail.com>
2024-11-27 15:04:22 +0000

    concatenates two arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""defines function 2D matrices containing ints/floats"""
+
+
+def cat_arrays(arr1, arr2):
+    """ Concatenates two arrays.
+    Args:
+        arr1 (list): The first array (list of ints/floats).
+        arr2 (list): The second array (list of ints/floats).
+    Returns:
+        list: A new list that is the concatenation of arr1 and arr2."""
+    return arr1 + arr2
