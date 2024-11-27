e2d647c551253c24f6f98637b0a0f3e3acdc9712
eftelakacori <eftelakacori92@gmail.com>
2024-11-27 21:12:52 +0000

    concatenates two matrices along a specific axis

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,13 +9,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
             return None
         # Return a new matrix with the rows concatenated
         return [row[:] for row in mat1] + [row[:] for row in mat2]
-
     elif axis == 1:  # Concatenate along columns
         # Check if both matrices have the same number of rows
         if len(mat1) != len(mat2):
             return None
         # Return a new matrix with the columns concatenated
         return [mat1[i] + mat2[i] for i in range(len(mat1))]
-
     # Return None for invalid axis
     return None
