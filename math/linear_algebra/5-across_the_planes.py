4af01dcca028c2d6818d7e05fd5841fbce42b376
eftelakacori <eftelakacori92@gmail.com>
2024-11-27 14:54:48 +0000

    5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -2,14 +2,7 @@
 """defines function 2D matrices containing ints/floats"""
 
 
-def add_matrices2D(mat1, mat2):
-    """Adds two 2D matrices element-wise.
-    Args:
-        mat1 (list of lists): The first 2D matrix (list of lists of ints/floats).
-        mat2 (list of lists): The second 2D matrix (list of lists of ints/floats).
-    Returns:
-        list of lists: A new matrix containing the element-wise sum of mat1 and mat2,
-                       or None if the matrices have different shapes."""
+def add_matrices2D(mat1, mat2):git
     # Kontrollo nëse matricat kanë të njëjtën formë
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None    
