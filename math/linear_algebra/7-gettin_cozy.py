3b36bff1ee16941519a63f8edde059203119221a
eftelakacori <eftelakacori92@gmail.com>
2024-11-27 22:04:45 +0000

    two matrices along a specific axis

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -2,16 +2,11 @@
 """defines function that calculates the shape of a matrix"""
 
 
-def cat_matrices2D(mat1, mat2, axis=0):
-    # Concatenate along rows (axis = 0)
+def cat_matrices2D(mat1, mat2, axis=0):  
+    """ Afunction that calculates the shape of a matrix"""
     if axis == 0:
-        # Check if the matrices have the same number of columns
-        if len(mat1[0]) == len(mat2[0]):
-            return mat1 + mat2  # Concatenate the matrices vertically
-    # Concatenate along columns (axis = 1)
+        return mat1.copy() + mat2.copy()
     elif axis == 1:
-        # Check if the matrices have the same number of rows
-        if len(mat1) == len(mat2):
-            return [mat1[i] + mat2[i] for i in range(len(mat1))]
-    # Return None if the matrices can't be concatenated
-    return None
+        #
+    else:
+        return None
