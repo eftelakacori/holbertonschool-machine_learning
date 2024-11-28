1729792c0efbe71776f62ea4bb15302a7bcedc50
eftelakacori <eftelakacori92@gmail.com>
2024-11-28 19:13:03 +0000

    5. Across The Planes

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
-"""defines function adds two matrices element-wise"""
+""" A script that adds two matrices element-wise """
 
 
 def add_matrices2D(mat1, mat2):
-    # Check if the matrices have the same number of rows and columns
+    """ A function that adds two matrices element-wise """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    
-    # Create a new matrix to store the result of the addition
+
     result = []
-    
-    # Iterate through the rows of the matrices
     for i in range(len(mat1)):
-        # Add corresponding elements in each row
         row = []
-        for j in range(len(mat1[i])):
+        for j in range(len(mat1[0])):
             row.append(mat1[i][j] + mat2[i][j])
         result.append(row)
-    
     return result
+    
