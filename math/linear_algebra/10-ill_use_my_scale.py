8fcf9e04b03595077c78a4da151d2712a3115c8f
eftelakacori <eftelakacori92@gmail.com>
2024-11-28 09:49:00 +0000

    I’ll Use My Scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""defines function that returns the transpose of a 2D matrix"""
+
+
+import numpy as np
+
+
+def np_shape(matrix):
+    # Directly return the shape of the matrix as a tuple of integers
+    return matrix.shape
+
+
+# Test matrices
+mat1 = np.array([1, 2, 3, 4, 5, 6])
+mat2 = np.array([])
+mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+             [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+
+# Outputting the results
+print(np_shape(mat1))  # Expected output: (6,)
+print(np_shape(mat2))  # Expected output: (0,)
+print(np_shape(mat3))  # Expected output: (2, 2, 5)
