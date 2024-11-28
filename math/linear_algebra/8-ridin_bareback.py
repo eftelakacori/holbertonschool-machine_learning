01780ab603e33bcf46300737fb2e0463ca5a92af
eftelakacori <eftelakacori92@gmail.com>
2024-11-28 19:31:28 +0000

    8. Ridin’ Bareback

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
-"""defines function that that performs matrix multiplications"""
+"""Write a function that performs matrix multiplication"""
 
 
 def mat_mul(mat1, mat2):
-    """Performs matrix multiplication with two given matrices"""
+    """
+    Performs matrix multiplication of two 2D matrices.
+
+    Args:
+        mat1: List of lists (2D matrix) containing ints/floats.
+        mat2: List of lists (2D matrix) containing ints/floats.
+
+    Returns:
+        A new 2D matrix that is the result of multiplying mat1 and mat2.
+        Returns None if the matrices cannot be multiplied.
+    """
+    # Validate if matrix multiplication is possible
+    # Number of columns in mat1 must equal number of rows in mat2
     if len(mat1[0]) != len(mat2):
         return None
 
-    mat = [[sum(mat1[z][j] * mat2[j][i] for j in range(len(mat1[0])))
-            for i in range(len(mat2[0]))] for z in range(len(mat1))]
+    # Perform matrix multiplication
+    # Result matrix dimensions will be len(mat1) x len(mat2[0])
+    result = [
+        [sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)]
+        for row in mat1
+    ]
 
-    return
+    return result
