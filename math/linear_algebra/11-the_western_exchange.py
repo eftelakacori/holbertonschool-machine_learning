a90520c097a6fc2c5ee5691aaa4df9f0e56e2239
eftelakacori <eftelakacori92@gmail.com>
2024-11-28 12:39:27 +0000

    The Western Exchange

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -3,5 +3,14 @@
 
 
 def np_transpose(matrix):
-    """Transposes a given matrix"""
+    """
+    Transposes the given numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): The matrix to be transposed.
+
+    Returns:
+        numpy.ndarray: A new matrix that is the transpose of the input matrix.
+    """
+    # Use the .T attribute of numpy arrays to perform the transpose operation
     return matrix.T
