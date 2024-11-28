044ee41af56671cc5413ade69f09de97a2ee4b8e
eftelakacori <eftelakacori92@gmail.com>
2024-11-28 19:16:48 +0000

    5. Across The Planes

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,7 +6,6 @@ def add_matrices2D(mat1, mat2):
     """ A function that adds two matrices element-wise """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-
     result = []
     for i in range(len(mat1)):
         row = []
@@ -14,4 +13,3 @@ def add_matrices2D(mat1, mat2):
             row.append(mat1[i][j] + mat2[i][j])
         result.append(row)
     return result
-    
