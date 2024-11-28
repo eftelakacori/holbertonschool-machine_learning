912380ddea96a05bb5b86fbfec5ebf45d9c24b49
eftelakacori <eftelakacori92@gmail.com>
2024-11-28 01:56:03 +0000

    Saddle Up

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,17 +2,16 @@
 """defines function that performs matrix multiplication"""
 
 
-iimport numpy as np
+import numpy as np
+
 
 def np_matmul(mat1, mat2):
     """
-    Krijon një matrice të re duke shumëzuar dy matrica të dhëna.
-    
-    Parametrat:
-    mat1 -- Matricha e parë (numpy.ndarray)
-    mat2 -- Matricha e dytë (numpy.ndarray)
-    
-    Kthen:
-    numpy.ndarray -- Matricha rezultuese nga shumëzimi i mat1 dhe mat2
+    Performs matrix multiplication between two given matrices.
+    Parameters:
+    mat1 -- The first matrix (numpy.ndarray)
+    mat2 -- The second matrix (numpy.ndarray)
+    Returns: numpy.ndarray --The resulting matrix from the
+    multiplication of mat1 and mat2
     """
     return np.matmul(mat1, mat2)
