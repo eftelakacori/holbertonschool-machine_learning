f2d9630d255b7b0f2e0833cf190149cda2d95fb3
eftelakacori <eftelakacori92@gmail.com>
2024-11-28 12:45:01 +0000

    10. I’ll Use My Scale

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -3,16 +3,14 @@
 
 
 def np_shape(matrix):
-    # Return the shape of the matrix as a tuple of integers
+    """
+    Calculates the shape of a numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): The input array.
+
+    Returns:
+        tuple: A tuple of integers representing the shape of the array.
+    """
+    # Return the shape of the matrix using the .shape attribute
     return matrix.shape
-
-# Test matrices
-mat1 = np.array([1, 2, 3, 4, 5, 6])  # 1D array with 6 elements
-mat2 = np.array([])  # Empty array
-mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], 
-                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])  # 3D array
-
-# Outputting the results
-print(np_shape(mat1))  # Expected output: (6,)
-print(np_shape(mat2))  # Expected output: (0,)
-print(np_shape(mat3))  # Expected output: (2, 2, 5)
