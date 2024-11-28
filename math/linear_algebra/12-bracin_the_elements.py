50e5ef7fb5a8d6f81015eba07ae295481d2c40a8
eftelakacori <eftelakacori92@gmail.com>
2024-11-28 12:14:30 +0000

    Bracing The Elements

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -2,12 +2,6 @@
 """defines function that Performs element-wise"""
 
 
-#!/usr/bin/env python3
-"""Write a function that performs element-wise addition, subtraction,
-multiplication, and division"""
-
-
 def np_elementwise(mat1, mat2):
-    """ Performs element-wise addition, subtraction, multiplication,
-    and division between two given matrices"""
-    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 * 1 / mat2
+    # Perform element-wise operations directly using numpy
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
