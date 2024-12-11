7c7d2bf7c878421b327e40803c021d8b9728151f
eftelakacori <eftelakacori92@gmail.com>
2024-12-11 12:33:08 +0000

    Flip it and Switch it

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
-""" a script that Write a function def flip_switch(df) takes a pd.DataFrame"""
+"""
+Task 6: sort and tranpose a pandas dataframe
+"""
 
 
-from_file = __import__('2-from_file').from_file
-flip_switch = __import__('6-flip_switch').flip_switch
-
-# Load data from the CSV file
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-# Call the flip_switch function
-df = flip_switch(df)
-
-# Print the last 8 rows of the transposed DataFrame
-print(df.tail(8))
+def flip_switch(df):
+    """ sort and tranpose a pandas dataframe """
+    df = df.sort_index(ascending=False).T
+    return df
