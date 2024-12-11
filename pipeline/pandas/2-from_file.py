479d1af95e7b1ee779e644b4ed24140eb33c599c
eftelakacori <eftelakacori92@gmail.com>
2024-12-11 09:10:17 +0000

    From File

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" a script that calculates the sum of squares"""
+
+import pandas as pd
+
+def from_file(filename, delimiter):
+    """Loads data from a file and returns a pandas DataFrame."""
+    # Load the CSV file into a pandas DataFrame
+    df = pd.read_csv(filename, delimiter=delimiter)
+    
+    return df
