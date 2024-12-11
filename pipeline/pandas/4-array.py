8f7ec36288575db122c3443deb2ecc71f7f7c0fe
eftelakacori <eftelakacori92@gmail.com>
2024-12-11 11:47:25 +0000

    To Numpy

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -9,6 +9,6 @@ def array(df):
     """Select the last 10 rows of 'High' and 'Close' columns."""
     # Select the last 10 rows of 'High' and 'Close' columns
     last_10_rows = df[['High', 'Close']].tail(10)
-
-# Convert the selected values to numpy.ndarray and return
+    
+    # Convert the selected values to numpy.ndarray and return
     return last_10_rows.to_numpy()
