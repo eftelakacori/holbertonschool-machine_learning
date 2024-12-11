b06bf7ef3f728d9baff495a3e6974b3849f846c3
eftelakacori <eftelakacori92@gmail.com>
2024-12-11 12:20:03 +0000

    To Numpy

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """ a script that calculates the sum of squares"""
 
 
@@ -9,6 +8,6 @@ def array(df):
     """Select the last 10 rows of 'High' and 'Close' columns."""
     # Select the last 10 rows of 'High' and 'Close' columns
     last_10_rows = df[['High', 'Close']].tail(10)
-    
+
     # Convert the selected values to numpy.ndarray and return
     return last_10_rows.to_numpy()
