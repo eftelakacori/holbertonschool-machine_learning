28c9d688feba0fc00d83e10f496655aea384a528
eftelakacori <eftelakacori92@gmail.com>
2024-12-11 09:11:30 +0000

    From File

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -3,9 +3,10 @@
 
 import pandas as pd
 
+
 def from_file(filename, delimiter):
     """Loads data from a file and returns a pandas DataFrame."""
     # Load the CSV file into a pandas DataFrame
     df = pd.read_csv(filename, delimiter=delimiter)
-    
+
     return df
