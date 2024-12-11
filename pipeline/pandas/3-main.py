03e20282e34c75a682395fef9cc50e586247b4a7
eftelakacori <eftelakacori92@gmail.com>
2024-12-11 09:17:09 +0000

    Rename

--- pipeline/pandas/3-main.py
+++ pipeline/pandas/3-main.py
@@ -3,8 +3,11 @@
 from_file = __import__('2-from_file').from_file
 rename = __import__('3-rename').rename
 
+# Load the data from the file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
+# Call the rename function to modify the DataFrame
 df = rename(df)
 
+# Print the last few rows of the modified DataFrame
 print(df.tail())
