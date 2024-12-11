b73c8aa982ac41a1af40a98ad17a8d1bc35402a7
eftelakacori <eftelakacori92@gmail.com>
2024-12-11 10:10:24 +0000

    Slice

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -2,17 +2,23 @@
 """ a script that calculates the sum of squares"""
 
 
+#!/usr/bin/env python3
+import numpy as np
+
+
 def array(df):
     """Selects the last 10 rows of the High and Close columns and converts them to numpy.ndarray."""
     # Select the last 10 rows of 'High' and 'Close' columns
     last_10_rows = df[['High', 'Close']].tail(10)
- 
+
     # Convert the selected rows to a numpy.ndarray
     return last_10_rows.to_numpy()
+
 
 def count_occurrences(df, column, value):
     """Counts the number of occurrences of a specific value in the specified column."""
     return (df[column] == value).sum()
+
 
 # Example usage
 if __name__ == "__main__":
