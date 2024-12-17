d0e838be8545b51ae74f6f8169dc4dce8d74777f
eftelakacori <eftelakacori92@gmail.com>
2024-12-17 13:45:52 +0000

    Indexing

--- pipeline/pandas/10-index.py
+++ pipeline/pandas/10-index.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-""" A script that sets the Timestamp column as the index. """
+"""
+Task 10
+"""
 
-import pandas as pd
 
 def index(df):
-    """Set the Timestamp column as the index of the DataFrame."""
-    # Set the 'Timestamp' column as the index
-    df.set_index('Timestamp', inplace=True)
-    
-    # Return the modified DataFrame
-    return df
+    """
+    """
+    return df.set_index('Timestamp')
