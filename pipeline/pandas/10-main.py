2743a7a44cab5f9ec99fda8b6805fcff8654a77b
eftelakacori <eftelakacori92@gmail.com>
2024-12-17 13:42:14 +0000

    Indexing

--- pipeline/pandas/10-main.py
+++ pipeline/pandas/10-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+"""
+Task 10
+"""
 
-from_file = __import__('2-from_file').from_file
-index = __import__('10-index').index
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-df = index(df)
-
-print(df.tail())
+def index(df):
+    """
+    """
+    return df.set_index('Timestamp')
