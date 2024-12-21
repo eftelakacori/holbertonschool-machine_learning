bcf4463f1ec254b54b370c26843cd943351b29be
eftelakacori <eftelakacori92@gmail.com>
2024-12-21 22:49:14 +0000

    line

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python3
+"""
+Task 0
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
-    # Generate y values as the cube of integers from 0 to 10
+    """
+    """
     y = np.arange(0, 11) ** 3
-    
-    # Set up the x values, which should match the length of y
-    x = np.arange(0, 11)
-
-    # Create the plot with a solid red line
-    plt.plot(x, y, color='red', linestyle='-', linewidth=2)  # Solid red line
-
-    # Set the x-axis range from 0 to 10
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(y, color='red')
     plt.xlim(0, 10)
-
-    # Set the title and labels
-    plt.title("Cubic Function")
-    plt.xlabel("X Axis")
-    plt.ylabel("Y Axis")
-
-    # Display the plot
     plt.show()
-
-# Call the function to display the plot
-line()
