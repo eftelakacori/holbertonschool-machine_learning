9eb6edbb4f34ebec53edc5e29dd303b68bf75245
eftelakacori <eftelakacori92@gmail.com>
2025-01-08 09:51:05 +0000

    Rate me is you can!

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -16,22 +16,15 @@ DEFAULT_HEADERS = {"Accept": "application/vnd.github.v3+json"}
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         exit()
-    
-    # Merr URL-në e dhënë si argument
     url = sys.argv[1]
-    
-    # Bën kërkesën për informacionet e përdoruesit nga GitHub API
     r = requests.get(url, DEFAULT_HEADERS)
 
-    # Kontrollon statusin e përgjigjes dhe printon vendndodhjen e përdoruesit
     if r.status_code == 200:
         print(r.json()["location"])
 
-    # Nëse përdoruesi nuk është gjetur (404)
     elif r.status_code == 404:
         print("Not found")
 
-    # Nëse është tejkaluar kufiri i kërkesave (403)
     elif r.status_code == 403:
         rate_limit = int(r.headers["X-Ratelimit-Reset"])
         now = int(time.time())
