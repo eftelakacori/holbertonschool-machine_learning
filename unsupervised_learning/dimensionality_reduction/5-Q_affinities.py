419722b5a3762113cb8d58d0fa23188e6dc1723e
eftelakacori <eftelakacori92@gmail.com>
2025-02-21 22:59:25 +0000

    Q affinities

--- unsupervised_learning/dimensionality_reduction/5-Q_affinities.py
+++ unsupervised_learning/dimensionality_reduction/5-Q_affinities.py
@@ -1,35 +1,26 @@
 #!/usr/bin/env python3
 """
-Module for calculating Q affinities in t-SNE dimensionality reduction.
-
-This module contains the function `Q_affinities`, which computes
-the Q affinities and their numerators based on the low-dimensional
-representation of data.
+Calculates the symmetric Q affinities
 """
-
 import numpy as np
 
 
 def Q_affinities(Y):
     """
-    Calculates the Q affinities for
-    the low-dimensional representation of X.
-
-    Parameters:
-      Y (numpy.ndarray): Shape (n, ndim),
-      the low-dimensional representation of X.
-
-    Returns:
-      Q (numpy.ndarray): Shape (n, n), the Q affinities matrix.
-      num (numpy.ndarray): Shape (n, n), the numerator of the Q affinities.
+    Calculates the Q affinities
+    :param Y: Y is a numpy.ndarray of shape (n, ndim) containing the low
+    dimensional transformation of X
+        n is the number of points
+        ndim is the new dimensional representation of X
+    :return: Q, num:
+        Q is a numpy.ndarray of shape (n, n) containing the Q affinities
+        num is a numpy.ndarray of shape (n, n) containing the numerator of the
+        Q affinities
     """
-    sum_Y = np.sum(np.square(Y), axis=1)
-    distances = np.add(np.add(-2 * np.dot(Y, Y.T), sum_Y).T, sum_Y)
-
-    num = 1 / (1 + distances)
+    sum_Y = np.sum(np.square(Y), 1)
+    num = -2 * np.dot(Y, Y.T)
+    num = 1 / (1. + np.add(np.add(num, sum_Y).T, sum_Y))
     np.fill_diagonal(num, 0)
-
-    denom = np.sum(num)
-    Q = num / denom
+    Q = num / np.sum(num)
 
     return Q, num
