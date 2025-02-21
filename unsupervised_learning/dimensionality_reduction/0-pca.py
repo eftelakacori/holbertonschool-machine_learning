52ded07bced00ed1dc2786de6bb79ebc44bbc289
eftelakacori <eftelakacori92@gmail.com>
2025-02-21 21:12:28 +0000

    0. PCA

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -7,36 +7,23 @@ import numpy as np
 
 def pca(X, var=0.95):
     """
-    Performs PCA on dataset X.
-
-    X is a numpy.ndarray of shape (n, d) where:
-      - n: number of data points
-      - d: number of dimensions in each point
-    All dimensions have mean 0.
-
-    var is the fraction of variance to maintain.
-    Returns:
-      W, a numpy.ndarray of shape (d, nd) where
-      nd is the new dimensionality that maintains
-      var fraction of X's variance.
+    Kryen PCA mbi datasetin X.
+    X: numpy.ndarray me formën (n, d) ku n është numri i
+       pikave dhe d numri i dimensioneve. Të gjitha kanë mesatare 0.
+    var: fraksioni i variancës që duhet ruajtur.
+    Kthen matricën W me formën (d, nd) ku nd është dimensionaliteti
+    i ri që ruan var-fraksionin e variancës.
     """
-    # Compute covariance matrix of X
-    cov_matrix = np.cov(X, rowvar=False)
-
-    # Compute eigenvalues and eigenvectors
-    eigvals, eigvecs = np.linalg.eigh(cov_matrix)
-
-    # Sort eigenvalues and eigenvectors in descending order
-    sorted_idx = np.argsort(eigvals)[::-1]
-    eigvals = eigvals[sorted_idx]
-    eigvecs = eigvecs[:, sorted_idx]
-
-    # Compute cumulative variance ratio
-    cumulative_var = np.cumsum(eigvals) / np.sum(eigvals)
-
-    """Find minimum number of components to
-    reach desired variance"""
-    nd = np.argmax(cumulative_var >= var) + 1
-
-    # Return top nd eigenvectors as the weights matrix
+    # Llogarit matricën e kovariancës
+    cov = np.cov(X, rowvar=False)
+    # Merr vlerat dhe vektorët eigen
+    eigvals, eigvecs = np.linalg.eigh(cov)
+    # Rendit në rend zbritës sipas vlerave eigen
+    idx = np.argsort(eigvals)[::-1]
+    eigvals = eigvals[idx]
+    eigvecs = eigvecs[:, idx]
+    # Llogarit variancën kumulative
+    cum_var = np.cumsum(eigvals) / np.sum(eigvals)
+    nd = np.argmax(cum_var >= var) + 1
+    # Kthen matricën e peshave për komponentët e zgjedhur
     return eigvecs[:, :nd]
