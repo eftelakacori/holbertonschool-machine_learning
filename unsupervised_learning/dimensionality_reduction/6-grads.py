8f881e9054fa6d67b7018adef53b55120c2dd44d
eftelakacori <eftelakacori92@gmail.com>
2025-02-21 23:00:32 +0000

    GRADS

--- unsupervised_learning/dimensionality_reduction/6-grads.py
+++ unsupervised_learning/dimensionality_reduction/6-grads.py
@@ -1,42 +1,28 @@
 #!/usr/bin/env python3
+"""
+Calculates the gradients
+"""
 import numpy as np
+Q_affinities = __import__('5-Q_affinities').Q_affinities
+
 
 def grads(Y, P):
     """
-    Calculates the gradients of Y and the Q affinities in the t-SNE algorithm.
-
-    Parameters:
-      Y: numpy.ndarray of shape (n, ndim)
-         The low-dimensional representation of X.
-      P: numpy.ndarray of shape (n, n)
-         The P affinities of X.
-
-    Returns:
-      dY: numpy.ndarray of shape (n, ndim)
-          The gradients of Y.
-      Q: numpy.ndarray of shape (n, n)
-          The Q affinities of Y.
+    Calculates the gradients of Y
+    :param Y: numpy.ndarray of shape (n, ndim) containing the low dimensional
+    transformation of X
+    :param P: numpy.ndarray of shape (n, n) containing the P affinities of X
+    :return: (dY, Q)
+        dY is a numpy.ndarray of shape (n, ndim) containing the gradients of Y
+        Q is a numpy.ndarray of shape (n, n) containing the Q affinities of Y
     """
-    # Import the Q_affinities function
-    Q_affinities = __import__('5-Q_affinities').Q_affinities
-    
-    # Compute Q affinities for Y
-    Q, num = Q_affinities(Y)  # Q has shape (n, n)
-    
-    # Ensure no division by zero (clip minimum value to avoid instability)
-    PQ_diff = P - Q
-    
-    # Compute pairwise squared Euclidean distances in Y
-    sum_Y = np.sum(np.square(Y), axis=1)
-    distances = np.add(np.add(-2 * np.dot(Y, Y.T), sum_Y).T, sum_Y)
+    (n, ndim) = Y.shape
+    dY = np.zeros((n, ndim))
+    Q, num = Q_affinities(Y)
+    PQ = P - Q
 
-    # Compute 1 / (1 + ||y_i - y_j||^2)
-    inv_distances = 1 / (1 + distances)
-
-    # Compute the differences (Y[i] - Y[j])
-    diff = Y[:, np.newaxis, :] - Y[np.newaxis, :, :]
-
-    # Compute gradients
-    dY = np.sum((PQ_diff * inv_distances)[:, :, np.newaxis] * diff, axis=1)
+    for i in range(n):
+        dY[i, :] = np.sum(
+            np.tile(PQ[:, i] * num[:, i], (ndim, 1)).T * (Y[i, :] - Y), 0)
 
     return dY, Q
