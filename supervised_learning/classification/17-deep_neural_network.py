1024913ba7f8fa252c81d0abdfac73390af5929c
eftelakacori <eftelakacori92@gmail.com>
2025-03-26 13:53:40 +0000

    Privatize DeepNeuralNetwork

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """
 Class that defines a deep neural network performing binary classification
@@ -48,4 +47,3 @@ class DeepNeuralNetwork:
     @property
     def weights(self):
         return (self.__weights)
-
