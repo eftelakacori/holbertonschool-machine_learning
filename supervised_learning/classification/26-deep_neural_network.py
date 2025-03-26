c2eb29c91805294a710d7bf977e988322631142f
eftelakacori <eftelakacori92@gmail.com>
2025-03-26 14:30:29 +0000

    Persistence is Key

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -3,7 +3,6 @@
 import numpy as np
 import pickle
 
-
 class DeepNeuralNetwork:
     """Deep neural network class"""
     def __init__(self, nx, layers):
@@ -16,16 +15,16 @@ class DeepNeuralNetwork:
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
-        self.__weights["W1"] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
+        self.__weights["W1"] = np.random.randn(layers[0], nx) * \
+            np.sqrt(2 / nx)
         self.__weights["b1"] = np.zeros([layers[0], 1], dtype=float)
         if layers[0] <= 0 or type(layers[0]) is not int:
             raise TypeError("layers must be a list of positive integers")
         for i in range(1, self.L):
             if layers[i] <= 0 or type(layers[i]) is not int:
                 raise TypeError("layers must be a list of positive integers")
-            self.__weights["W{}".format(
-                i + 1)] = np.random.randn(layers[i],
-                                          layers[i-1])*np.sqrt(2/layers[i-1])
+            self.__weights["W{}".format(i + 1)] = np.random.randn(
+                layers[i], layers[i-1]) * np.sqrt(2/layers[i-1])
             self.__weights["b{}".format(i + 1)] = np.zeros([layers[i], 1], dtype=float)
 
     @property
