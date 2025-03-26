e1d67a614e85f9cff26ec1b3f7d671bbbeeb96b3
eftelakacori <eftelakacori92@gmail.com>
2025-03-26 13:52:02 +0000

    Privatize DeepNeuralNetwork

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,32 +1,51 @@
+
 #!/usr/bin/env python3
 """
-a class that defines a deep neural network performing binary classification
+Class that defines a deep neural network performing binary classification
 """
 import numpy as np
 
 
 class DeepNeuralNetwork:
     """
-    a class that defines a deep neural network performing binary classification
+    class that represents a deep neural network
+    performing binary classification
     """
     def __init__(self, nx, layers):
         """class constructor"""
-        if type(nx) != int:
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) != list or len(layers) == 0:
+        if type(layers) is not list or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
-        for i, n in enumerate(layers):
-            if type(n) != int or n < 1:
+
+        weights = {}
+        previous = nx
+
+        for index, layer in enumerate(layers, 1):
+
+            if type(layer) is not int or layer < 0:
                 raise TypeError("layers must be a list of positive integers")
-            if i == 0:
-                self.weights = {"W1": np.random.randn(n, nx) * np.sqrt(2 / nx),
-                                "b1": np.zeros((n, 1))}
-            else:
-                self.weights["W{}".format(i + 1)] = \
-                    np.random.randn(n, layers[i - 1]) \
-                    * np.sqrt(2 / layers[i - 1])
-                self.weights["b{}".format(i + 1)] = np.zeros((n, 1))
-        self.L = len(layers)
-        self.cache = {}
+
+            weights["b{}".format(index)] = np.zeros((layer, 1))
+            weights["W{}".format(index)] = (np.random.randn(layer, previous)
+                                            * np.sqrt(2 / previous))
+            previous = layer
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = weights
+
+    @property
+    def L(self):
+        return (self.__L)
+
+    @property
+    def cache(self):
+        return (self.__cache)
+
+    @property
+    def weights(self):
+        return (self.__weights)
+
