cfeec28ac894c6319aa4b9be7d37b7f7a66f9f48
eftelakacori <eftelakacori92@gmail.com>
2025-04-19 17:05:06 +0000

    Momentum

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -1,72 +1,26 @@
 #!/usr/bin/env python3
 """
-class that represents a single neuron performing binary classification
+   Momentum
 """
-import numpy as np
 
 
-class Neuron:
+def update_variables_momentum(alpha, beta1, var, grad, v):
     """
-    class that represents a single neuron performing binary classification
+        method that updates a variable using the gradient descent
+        with momentum optimization algorithm
+
+        :param alpha: learning rate
+        :param beta1: momentum weight
+        :param var: ndarray, variable to be updated
+        :param grad: ndarray, gradient of var
+        :param v: previous first moment of var
+
+        :return: updated variable and the new moment
     """
-    def __init__(self, nx):
-        if type(nx) is not int:
-            raise TypeError("nx must be an integer")
-        if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        self.__W = np.random.randn(1, nx)
-        self.__b = 0
-        self.__A = 0
+    # formula for momentum
+    dW = beta1 * v + (1 - beta1) * grad
 
-    @property
-    def W(self):
-        return (self.__W)
+    # update var
+    var_new = var - dW * alpha
 
-    @property
-    def b(self):
-        return (self.__b)
-
-    @property
-    def A(self):
-        return (self.__A)
-
-    def forward_prop(self, X):
-        """
-        calculates the forward propagation of the neuron
-        """
-        z = np.matmul(self.W, X) + self.b
-        self.__A = 1 / (1 + (np.exp(-z)))
-        return (self.A)
-
-    def cost(self, Y, A):
-        """
-        calculates the cost of the model using logistic regression function:
-            loss = -((Y * log(A)) + ((1 - Y) * log(1 - A)))
-        logistic regression cost function:
-            cost = (1 / m) * sum of loss function for all m example
-        """
-        m = Y.shape[1]
-        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
-        cost = (1 / m) * (-(m_loss))
-        return (cost)
-
-    def evaluate(self, X, Y):
-        """
-        evaluates the neuron's predictions
-        """
-        A = self.forward_prop(X)
-        cost = self.cost(Y, A)
-        prediction = np.where(A >= 0.5, 1, 0)
-        return (prediction, cost)
-
-    def gradient_descent(self, X, Y, A, alpha=0.05):
-        """
-        calculates one pass of gradient descent on the neuron
-        derivative of loss function with respect to A
-        """
-        m = Y.shape[1]
-        dz = (A - Y)
-        d__W = (1 / m) * (np.matmul(X, dz.transpose()).transpose())
-        d__b = (1 / m) * (np.sum(dz))
-        self.__W = self.W - (alpha * d__W)
-        self.__b = self.b - (alpha * d__b)
+    return var_new, dW
