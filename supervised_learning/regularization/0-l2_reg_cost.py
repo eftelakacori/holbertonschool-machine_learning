b2932c6066ac358091f8ed166a1d6e9856fae7c5
eftelakacori <eftelakacori92@gmail.com>
2025-05-06 07:14:40 +0000

    L2 Regularization Cost

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -5,13 +5,13 @@ A function that calculates the cost of a neural network with L2 regularization
 import numpy as np
 
 
-def l2_reg_cost(cost, lambtha, weights, L, m):
+def L2_reg_cost(cost, lambtha, weights, L, m):
     """
     A function that calculates the cost of a neural network
     with L2 regularization
     """
-    l2_norm_squared = 0
-    for l in range(1, L+1):
-        l2_norm_squared += np.sum(np.square(weights["W" + str(l)]))
-    l2_regularization_cost = (lambtha / (2 * m)) * l2_norm_squared
-    return cost + l2_regularization_cost
+    L2_norm_squared = 0
+    for L in range(1, L+1):
+        L2_norm_squared += np.sum(np.square(weights["W" + str(L)]))
+    L2_regularization_cost = (lambtha / (2 * m)) * L2_norm_squared
+    return cost + L2_regularization_cost
