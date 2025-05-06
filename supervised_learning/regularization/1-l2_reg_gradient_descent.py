2f40c7af2520696fc4382ca162289998b96f7e8c
eftelakacori <eftelakacori92@gmail.com>
2025-05-06 07:20:30 +0000

    Gradient Descent with L2 Regularization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,20 +1,44 @@
 #!/usr/bin/env python3
-"""A function that updates the weights and biases of a neural network
-using gradient descent with L2 regularization"""
+"""
+    Gradient Descent with L2 regularization
+"""
+
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """A function that updates the weights and biases of a neural network
-    using gradient descent with L2 regularization"""
+    """
+        function that updates the weights and biases of NN
+        using gradient descent with L2 regularization
+
+        Formula:
+        W = W - alpha * (dW + (lambtha/m) * W)
+    """
+    # store m number training examples
     m = Y.shape[1]
-    dZ = cache["A" + str(L)] - Y
-    for l in range(L, 0, -1):
-        A_prev = cache["A" + str(l-1)]
-        W = weights["W" + str(l)]
-        dW = (1/m) * np.dot(dZ, A_prev.T) + (lambtha/m) * W
-        db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
-        dA_prev = np.dot(W.T, dZ)
-        dZ = dA_prev * (1 - np.square(A_prev))
-        weights["W" + str(l)] -= alpha * dW
-        weights["b" + str(l)] -= alpha * db
+
+    # derivative of final layer
+    # starting point for backpropagation
+    dZ = cache['A' + str(L)] - Y
+
+    # backpropagation loop
+    for layer in range(L, 0, -1):
+        L2_regularization = lambtha / m * weights['W' + str(layer)]
+
+        # activation of previous layer
+        A_prev = cache['A' + str(layer - 1)]
+
+        # Gradient of Weight and biases
+        dW = np.matmul(dZ, A_prev.T) / m + L2_regularization
+        db = np.sum(dZ, axis=1, keepdims=True) / m
+        dA = np.matmul(weights['W' + str(layer)].T, dZ)
+
+        # derivative of tanh activation
+        if layer != 1:
+            dZ = dA * (1 - A_prev ** 2)
+        else:  # Apply softmax derivative for the last layer
+            dZ = dA
+
+        # update weights
+        weights['W' + str(layer)] -= alpha * dW
+        weights['b' + str(layer)] -= alpha * db
