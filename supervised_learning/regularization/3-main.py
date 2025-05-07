34e9360c76a1e4a0947e3728935e9e5fef159fe7
eftelakacori <eftelakacori92@gmail.com>
2025-05-07 13:54:28 +0000

    Create a Layer with L2 Regularization

--- supervised_learning/regularization/3-main.py
+++ supervised_learning/regularization/3-main.py
@@ -1,43 +1,26 @@
 #!/usr/bin/env python3
+"""
+Creates a layer with L2 regularization
+"""
 
-import numpy as np
 import tensorflow as tf
-import os
-import random
 
-SEED = 0
 
-os.environ['PYTHONHASHSEED'] = str(SEED)
-os.environ['TF_ENABLE_ONEDNN_OPTS']= '0'
-random.seed(SEED)
-np.random.seed(SEED)
-tf.random.set_seed(SEED)
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Creates a TensorFlow layer that includes L2 regularization
 
-l2_reg_cost = __import__('2-l2_reg_cost').l2_reg_cost
-l2_reg_create_layer = __import__('3-l2_reg_create_layer').l2_reg_create_layer
+    Parameters:
+    - prev: tensor containing the output of the previous layer
+    - n: number of nodes in the new layer
+    - activation: activation function for the layer
+    - lambtha: L2 regularization parameter
 
-def one_hot(Y, classes):
-    """convert an array to a one-hot matrix"""
-    m = Y.shape[0]
-    one_hot = np.zeros((m, classes))
-    one_hot[np.arange(m), Y] = 1
-    return one_hot
-
-lib= np.load('MNIST.npz')
-X_train_3D = lib['X_train']
-Y_train = lib['Y_train']
-X_train = X_train_3D.reshape((X_train_3D.shape[0], -1))
-Y_train_oh = one_hot(Y_train, 10)
-
-input_shape = X_train.shape[1]
-
-x = tf.keras.Input(shape=input_shape)
-h1 = l2_reg_create_layer(x, 256, tf.nn.tanh, 0.05)  
-y_pred = l2_reg_create_layer(h1, 10, tf.nn.softmax, 0.)   
-model = tf.keras.Model(inputs=x, outputs=y_pred)
-
-Predictions = model(X_train)
-cost = tf.keras.losses.CategoricalCrossentropy()(Y_train_oh, Predictions)
-
-l2_cost = l2_reg_cost(cost,model)
-print(l2_cost)
+    Returns:
+    - The output tensor of the created layer
+    """
+    regularizer = tf.keras.regularizers.L2(lambtha)
+    layer = tf.keras.layers.Dense(units=n,
+                                  activation=activation,
+                                  kernel_regularizer=regularizer)
+    return layer(prev)
