83e77eefbf10e32ea57275bbbe3d610a6716f666
eftelakacori <eftelakacori92@gmail.com>
2025-05-09 06:59:58 +0000

    Create a Layer with L2 Regularization

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,27 +1,28 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 """
     Create layer with L2 regularization
 """
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-        Function that creates a tensorflow layer with L2 regularization
+        Function that creates a tensorflow layer includes L2 regularization
     """
-    # Set initialization to He et al.
-    initializer = tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
+    # set initialization to He et. al
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                        mode='fan_avg')
 
-    # Create the Dense layer with L2 regularization
-    new_layer = tf.keras.layers.Dense(
-        units=n,
-        activation=activation,
-        kernel_initializer=initializer,
-        kernel_regularizer=tf.keras.regularizers.l2(lambtha),
-        name="layer"
-    )
+    # create layer Dense with parameters
+    new_layer = (
+        tf.layers.Dense(n,
+                        activation=activation,
+                        kernel_initializer=initializer,
+                        kernel_regularizer=tf.keras.regularizers.l2(lambtha),
+                        name="layer"))
 
-    # Apply the layer to the input
+    # apply layer to input
     output = new_layer(prev)
 
     return output
