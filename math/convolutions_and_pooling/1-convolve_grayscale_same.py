35e7a0c45e77bf8d318eec1fda730d4ff64e25b6
eftelakacori <eftelakacori92@gmail.com>
2025-05-17 09:16:13 +0000

    Same Convolution

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import numpy as np
+"""Performs a same convolution on grayscale images"""
+
+
+def convolve_grayscale_same(images, kernel):
+    """
+    Performs a valid convolution on grayscale images
+    """
+    m, height, width = images.shape
+    kh, kw = kernel.shape
+    if (kh % 2) == 1 and (kw % 2) == 1:
+        ph = (kh - 1) // 2
+        pw = (kw - 1) // 2
+    else:
+        ph = kh // 2
+        pw = kw // 2
+
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
+    convoluted = np.zeros((m, height, width))
+    for h in range(height):
+        for w in range(width):
+            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel, axis=1)\
+                       .sum(axis=1)
+            convoluted[:, h, w] = output
+    return convoluted
