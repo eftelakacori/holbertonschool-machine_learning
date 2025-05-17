f8ac52f0e117c0e4af73008b6b78214b23ecda98
eftelakacori <eftelakacori92@gmail.com>
2025-05-17 09:26:04 +0000

    Same Convolution

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,26 +1,48 @@
 #!/usr/bin/env python3
+"""
+    Same Convolution
+"""
+
 import numpy as np
-"""Performs a same convolution on grayscale images"""
 
 
 def convolve_grayscale_same(images, kernel):
     """
-    Performs a valid convolution on grayscale images
-    """
-    m, height, width = images.shape
-    kh, kw = kernel.shape
-    if (kh % 2) == 1 and (kw % 2) == 1:
-        ph = (kh - 1) // 2
-        pw = (kw - 1) // 2
-    else:
-        ph = kh // 2
-        pw = kw // 2
+        Function that performs a same convolution on grayscale images
 
-    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
-    convoluted = np.zeros((m, height, width))
-    for h in range(height):
-        for w in range(width):
-            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel, axis=1)\
-                       .sum(axis=1)
-            convoluted[:, h, w] = output
-    return convoluted
+        :param images: ndarray, shape(m, h, w), multiple grayscale images
+        :param kernel: ndarray, shape(kh,kw), kernel for convolution
+
+        :return: ndarray containing convolved images
+    """
+    # size images and kernel
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    # output size
+    output_height = h
+    output_width = w
+
+    # initialize output
+    convolved_images = np.zeros((m, output_height, output_width))
+
+    # calcul padding (size odd or even)
+    padding_width = int(kw / 2)
+    padding_height = int(kh / 2)
+
+    # add zero padding to the input images
+    image_pad = np.pad(images,
+                       ((0, 0), (padding_height, padding_height),
+                        (padding_width, padding_width)), mode='constant')
+
+    # convolution
+    for i in range(output_height):
+        for j in range(output_width):
+            # extract region from each image
+            image_zone = image_pad[:, i:i+kh, j:j+kw]
+
+            # element wize multiplication
+            convolved_images[:, i, j] = np.sum(image_zone * kernel,
+                                               axis=(1, 2))
+
+    return convolved_images
