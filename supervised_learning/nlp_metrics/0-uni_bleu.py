27e5d5981a62c25880472f10a395be42abe0fd2b
eftelakacori <eftelakacori92@gmail.com>
2025-07-27 08:01:45 +0000

    Unigram BLEU score

---
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""A function that calculates the unigram BLEU score for a sentence"""
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """A function that calculates the unigram BLEU score for a sentence"""
+    unique = list(set(sentence))
+    words_dict = {}
+    for reference in references:
+        for word in reference:
+            if word in unique:
+                if word not in words_dict.keys():
+                    words_dict[word] = reference.count(word)
+                else:
+                    actual = reference.count(word)
+                    prev = words_dict[word]
+                    words_dict[word] = max(actual, prev)
+
+    candidate = len(sentence)
+    prob = sum(words_dict.values()) / candidate
+
+    best_match = []
+    for reference in references:
+        ref_len = len(reference)
+        diff = abs(ref_len - candidate)
+        best_match.append((diff, ref_len))
+
+    sort_tuple = sorted(best_match, key=(lambda x: x[0]))
+    best = sort_tuple[0][1]
+    if candidate > best:
+        bleu = 1
+    else:
+        bleu = np.exp(1 - (best / candidate))
+    score = bleu * np.exp(np.log(prob))
+    return score
